fda47896372ec385fa5621ca0c214ae5435ad65b
tatanalosinina <tatana@localhost>
2020-06-06 16:04:44 +0300

    fix2

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -44,13 +44,22 @@ class BonusServiceTest {
         long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
     }
-  
+
     @org.junit.jupiter.api.Test
     void shouldCalculateForNoRegisteredAndOverLimitmin500() {
         BonusService service = new BonusService();
         long amount = 5000_000;
         boolean registered = false;
         long expected = 500;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForNoRegisteredAndOverLimitmin1() {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
         long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
     }
